7c2472c468f14758e8f8dc993e27fd47c59773ca
Ibnwasar <ibnwasar@gmail.com>
2022-09-11 11:19:43 -0700

    my advance work

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - print the string in the print function
+*
+* Description: using the main function
+* this program prints "Programming is like building a multiligual puzzle
+* Return: 0
+*/
+int main(void)
+{
+	char c;
+	int i;
