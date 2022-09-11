283d58cbd354da71b884f610d5db7d91296b48af
Ibnwasar <ibnwasar@gmail.com>
2022-09-11 12:41:10 -0700

    longone

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,10 +4,21 @@
 * main - print the string in the print function
 *
 * Description: using the main function
-* this program prints "Programming is like building a multiligual puzzle
+* this program prints "Programming is like building a multilingual puzzle
 * Return: 0
 */
 int main(void)
 {
 	char c;
 	int i;
+	long li;
+	long long lli;
+	float f;
+
+	printf("int: %ld byte(s)\n", sizeof(c));
+	printf("int: %ld byte(s)\n", sizeof(i));
+	printf("long int: %ld byte(s)\n", sizeof(li));
+	printf("long long int: %ld byte(s)\n", sizeof(lli));
+	printf("float: %ld byte(s)\n", sizeof(f));
+	return (0);
+}
