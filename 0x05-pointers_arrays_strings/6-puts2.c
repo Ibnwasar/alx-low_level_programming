fa4859b8d00b03ea9bdb18a286d0c8907a29e597
Ibnwasar <ibnwasar@gmail.com>
2022-09-19 04:28:13 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,40 +1,18 @@
 #include "main.h"
 /**
-*rev_string - prints a reverse string
-*@s: A pointer to an int that will be changed
-*
-*Returned: void
+*puts2 - this function print only the even numbers
+*@str: is the principal parameter
+*Return: Always 0
 */
-
-void rev_string(char *s)
+void puts2(char *str)
 {
-char *start_c, *end_c, c;
-int i, count;
-int length = 0;
+int i = 0;
 
-for (i = 0; s[i]; i++)
+while (str[i] != 0)
 {
-length++;
+if ((i % 2) == 0)
+_putchar (str[i]);
+i++;
 }
-
-count = length;
-
-start_c = s;
-end_c = s;
-
-for (i = 0; i < count - 1; i++)
-{
-end_c++;
-}
-
-for (i = 0; i < count / 2; i++)
-{
-
-c = *end_c;
-*end_c = *start_c;
-*start_c = c;
-
-start_c++;
-end_c--;
-}
+_putchar('\n');
 }
