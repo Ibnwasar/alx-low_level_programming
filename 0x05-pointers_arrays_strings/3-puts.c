066bc182c4cdee811b03489150a962f5f12fb249
Ibnwasar <ibnwasar@gmail.com>
2022-09-19 03:21:04 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*_puts - prints a string
+*@str: A pointer to an int that will be changed
+*
+*Return: void which means our answer is correct
+*/
+
+void d _pu:(char *str)
+{
+char **C'
+int:wq
