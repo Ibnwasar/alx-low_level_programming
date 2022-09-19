db31b4bc277eb4403caed911579ebde92a42f4f3
Ibnwasar <ibnwasar@gmail.com>
2022-09-19 09:37:10 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <unistd.h>
 
 /**
-* main - Prints "add that piece of art is useful"-Dora Korpar, 2015-10-19",
+* main - Prints "add that piece of art is useful" - Dora Korpar, 2015-10-19",
 *	followed by a new line, to standard error.
 */
 int main(void)
 {
-	write(2, "and that piece of art is useful\"-Dora Korpar, 2015-10-19\n", 59);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 return (1);
 }
