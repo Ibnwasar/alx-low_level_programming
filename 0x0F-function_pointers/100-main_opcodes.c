8bd327efbd79f1f95605d908dca654709346b34f
Ibnwasar <ibnwasar@gmail.com>
2022-10-13 15:31:18 -0700

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -7,7 +7,6 @@
  * @argv: array of arguments
  *
  * Return: Always 0 (Success)
-
  */
 int main(int argc, char *argv[])
 {
@@ -19,21 +18,23 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(1);
 	}
+
 	bytes = atoi(argv[1]);
+
 	if (bytes < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
 
-	rr = (char *)main;
-
+	arr = (char *)main;
 	for (i = 0; i < bytes; i++)
 	{
 		if (i == bytes - 1)
 		{
 			printf("%02hhx\n", arr[i]);
 			break;
+
 		}
 		printf("%02hhx ", arr[i]);
 	}
