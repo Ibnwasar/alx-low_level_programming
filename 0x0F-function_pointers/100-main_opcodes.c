a3f9bab6f2bd38c4fc501ac281911cce2a9e6b8e
Ibnwasar <ibnwasar@gmail.com>
2022-10-13 21:19:20 -0700

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -28,17 +28,15 @@ int main(int argc, char *argv[])
 	}
 
 	arr = (char *)main;
+
 	for (i = 0; i < bytes; i++)
 	{
 		if (i == bytes - 1)
 		{
 			printf("%02hhx\n", arr[i]);
 			break;
-
 		}
 		printf("%02hhx ", arr[i]);
 	}
 	return (0);
 }
-Footer
-© 2022 GitHub
